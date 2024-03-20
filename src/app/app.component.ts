6dd40891152254f156d89f17fbf113ab3e886dca
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-03-20 14:56:13 -0500

    se agrego el sidenav al html `principal con su modulo en el componet.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BodyComponent } from './body/body.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,BodyComponent],
+  imports: [RouterOutlet, BodyComponent, SidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
