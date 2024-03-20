6ca33b03908cd9bd7ee90fb3cc66c67085e3a606
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-03-20 14:49:09 -0500

    modifcaciones  y mannejo de modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BodyComponent } from './body/body.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
