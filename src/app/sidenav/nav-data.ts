4f8862052a13e79d96e329e21d8543aaf92e0012
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-03-20 16:44:43 -0500

    se agergo un compoente dasboard se edito el html de sidenv

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,7 @@
+export const navbarData = [
+    {
+        routerLink: 'dashboard',
+        icon: 'bx bxs-dashboard',
+        label: 'Inicio'
+    }
+]
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { navbarData } from './nav-data';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
 
+  collapsed  = false;
+  navData = navbarData;
 }
