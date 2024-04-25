9db6224c4f700f78c07f0bc64dbb7978dac3a2ec
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-04-24 22:27:24 -0500

    se modifico tema nocturno

--- src/app/pointsatar/pointsatar.component.ts
+++ src/app/pointsatar/pointsatar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pointsatar',
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './pointsatar.component.html',
   styleUrl: './pointsatar.component.css'
 })
-export class PointsatarComponent {
+export class PointsatarComponent implements OnInit {
+  darkTheme = 'dark-theme';
+  iconTheme = 'uil-sun';
 
+  constructor() { }
+
+  ngOnInit(): void {
+    const selectedTheme = localStorage.getItem('selected-theme');
+    const selectedIcon = localStorage.getItem('selected-icon');
+
+    const getCurrentTheme = () => document.body.classList.contains(this.darkTheme) ? 'dark' : 'light';
+    const getCurrentIcon = () => document.getElementById('theme-button')?.classList.contains(this.iconTheme) ? 'uil-moon' : 'uil-sun';
+
+    if (selectedTheme) {
+      document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](this.darkTheme);
+      document.getElementById('theme-button')?.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](this.iconTheme);
+    }
+  }
+
+  toggleTheme(): void {
+    document.body.classList.toggle(this.darkTheme);
+    document.getElementById('theme-button')?.classList.toggle(this.iconTheme);
+
+    localStorage.setItem('selected-theme', this.getCurrentTheme());
+    localStorage.setItem('selected-icon', this.getCurrentIcon());
+  }
+
+  getCurrentTheme(): string {
+    return document.body.classList.contains(this.darkTheme) ? 'dark' : 'light';
+  }
+
+  getCurrentIcon(): string {
+    return document.getElementById('theme-button')?.classList.contains(this.iconTheme) ? 'uil-moon' : 'uil-sun';
+  }
 }
