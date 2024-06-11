b6ddd08024fac924a61a045bed5d5d5072c87124
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-06-10 21:56:51 -0500

    modificaciones disño de resumecambios de diseño

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-resume',
-  standalone: true,
-  imports: [],
   templateUrl: './resume.component.html',
-  styleUrl: './resume.component.css'
+  styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent {
+export class ResumeComponent implements AfterViewInit {
+  @ViewChild('wrapper') wrapper!: ElementRef;
+  @ViewChild('ticket') ticket!: ElementRef;
 
+  width!: number;
+  height!: number;
+  halfWidth!: number;
+  halfHeight!: number;
+
+  ngAfterViewInit() {
+    const wrapperElement = this.wrapper.nativeElement;
+    const { width, height } = wrapperElement.getBoundingClientRect();
+    this.width = width;
+    this.height = height;
+    this.halfWidth = width / 2;
+    this.halfHeight = height / 2;
+  }
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const offsetX = event.offsetX;
+    const offsetY = event.offsetY;
+
+    const rotationX = ((offsetX - this.halfWidth) / this.halfWidth) * 20;
+    const rotationY = ((offsetY - this.halfHeight) / this.halfHeight) * 20;
+
+    const ticketElement = this.ticket.nativeElement;
+    ticketElement.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+    ticketElement.style.transition = 'transform 0.3s';
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    const ticketElement = this.ticket.nativeElement;
+    ticketElement.style.transform = 'rotateX(0deg) rotateY(0deg)';
+  }
 }
