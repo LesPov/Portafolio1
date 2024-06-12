ce9a5b5403f7aff68d21709b9758fa6df5e8bcb0
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-06-12 00:16:02 -0500

    se agrego card de hailidades ene skills

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,44 +1,41 @@
-import { Component, ElementRef, HostListener, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-resume',
+  standalone: true,
+  imports: [],
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent implements AfterViewInit {
-  @ViewChild('wrapper') wrapper!: ElementRef;
-  @ViewChild('ticket') ticket!: ElementRef;
-
-  width!: number;
-  height!: number;
-  halfWidth!: number;
-  halfHeight!: number;
+  @ViewChild('wrapper', { static: false }) wrapperRef!: ElementRef<HTMLDivElement>;
+  @ViewChild('ticket', { static: false }) ticketRef!: ElementRef<HTMLDivElement>;
 
   ngAfterViewInit() {
-    const wrapperElement = this.wrapper.nativeElement;
-    const { width, height } = wrapperElement.getBoundingClientRect();
-    this.width = width;
-    this.height = height;
-    this.halfWidth = width / 2;
-    this.halfHeight = height / 2;
-  }
+    const wrapper = this.wrapperRef.nativeElement;
+    const ticket = this.ticketRef.nativeElement;
 
-  @HostListener('mousemove', ['$event'])
-  onMouseMove(event: MouseEvent) {
-    const offsetX = event.offsetX;
-    const offsetY = event.offsetY;
+    fromEvent(wrapper, 'mouseenter')
+      .subscribe(() => {
+        const { width, height } = wrapper.getBoundingClientRect();
+        const halfWidth = width / 2;
+        const halfHeight = height / 2;
 
-    const rotationX = ((offsetX - this.halfWidth) / this.halfWidth) * 20;
-    const rotationY = ((offsetY - this.halfHeight) / this.halfHeight) * 20;
+        const handleMouseMove = (event: MouseEvent) => {
+          const { offsetX, offsetY } = event;
+          const rotationX = ((offsetX - halfWidth) / halfWidth) * 30;
+          const rotationY = ((offsetY - halfHeight) / halfHeight) * 20;
+          ticket.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+          ticket.style.transition = 'transform 0.3s';
+        };
 
-    const ticketElement = this.ticket.nativeElement;
-    ticketElement.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
-    ticketElement.style.transition = 'transform 0.3s';
-  }
+        const handleMouseLeave = () => {
+          ticket.style.transform = 'rotateX(0deg) rotateY(0deg)';
+        };
 
-  @HostListener('mouseleave')
-  onMouseLeave() {
-    const ticketElement = this.ticket.nativeElement;
-    ticketElement.style.transform = 'rotateX(0deg) rotateY(0deg)';
+        wrapper.addEventListener('mousemove', handleMouseMove);
+        wrapper.addEventListener('mouseleave', handleMouseLeave);
+      });
   }
 }
