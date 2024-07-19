46a40d706de9cac59c35a9728e09549c8e4d9053
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-07-18 19:16:05 -0500

    se creo la carta falta mas cosas por agregar

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -10,11 +10,16 @@ import { fromEvent } from 'rxjs';
 export class ResumeComponent implements AfterViewInit {
   @ViewChildren('wrapper1, wrapper2, wrapper3, wrapper4, wrapper5, wrapper6') wrapperRefs!: QueryList<ElementRef<HTMLDivElement>>;
   @ViewChildren('ticket1, ticket2, ticket3, ticket4, ticket5, ticket6') ticketRefs!: QueryList<ElementRef<HTMLDivElement>>;
+  @ViewChildren('card1') cardRefs!: QueryList<ElementRef<HTMLDivElement>>;
 
   ngAfterViewInit() {
     this.wrapperRefs.forEach((wrapperRef, index) => {
       const ticketRef = this.ticketRefs.toArray()[index];
       this.addHoverEffect(wrapperRef, ticketRef);
+    });
+
+    this.cardRefs.forEach((cardRef) => {
+      this.addCardHoverEffect(cardRef);
     });
   }
 
@@ -37,6 +42,26 @@ export class ResumeComponent implements AfterViewInit {
       };
       wrapper.addEventListener('mousemove', handleMouseMove);
       wrapper.addEventListener('mouseleave', handleMouseLeave);
+    });
+  }
+
+  addCardHoverEffect(cardRef: ElementRef<HTMLDivElement>) {
+    const card = cardRef.nativeElement;
+
+    const handleMouseMove = (event: MouseEvent) => {
+      const rect = card.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+
+      // Adjust for the center of the card
+      card.style.setProperty('--x', `${x}px`);
+      card.style.setProperty('--y', `${y}px`);
+    };
+
+    card.addEventListener('mousemove', handleMouseMove);
+    card.addEventListener('mouseleave', () => {
+      card.style.setProperty('--x', `50%`); // Center position when mouse leaves
+      card.style.setProperty('--y', `50%`); // Center position when mouse leaves
     });
   }
 }
